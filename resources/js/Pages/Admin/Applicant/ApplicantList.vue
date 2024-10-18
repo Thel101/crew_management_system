727b6284414c67b5e01ecff102b51240e3f914b6
Phyu Thet <phyumyeon@gmail.com>
2024-10-18 14:32:05 +0630

    assign button fix

--- resources/js/Pages/Admin/Applicant/ApplicantList.vue
+++ resources/js/Pages/Admin/Applicant/ApplicantList.vue
@@ -157,7 +157,7 @@ watch(applicantUrl, newUrl => {
                                         <!-- :href="route('assign.seafarer', applicant.id, applicant.role.id) -->
                                         <Link :href="route('assign.seafarer', [applicant.id, applicant.role.id])"
                                             class="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700">
-                                        Assign {{ applicant.role.id }}
+                                        Assign
                                         </Link>
                                     </div>
 
